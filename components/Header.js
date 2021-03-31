5e253294696241ef54ab3f2b00ef84f5a3ef8558
benrickken <benrickken@gmail.com>
2021-03-31 22:37:53 +0900

    Add sign up link to header

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import Link from 'next/Link'
 import styles from './Header.module.scss'
 
 export default function Header() {
@@ -5,13 +6,17 @@ export default function Header() {
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.logo}>
-          <img src='/logo.svg' alt='Crowdfunding Logo' />
+          <Link href='/'>
+            <a>
+              <img src='/logo.svg' alt='Crowdfunding Logo' />
+            </a>
+          </Link>
         </div>
         <nav className={styles.headerNav}>
           <ul>
-            <li>
-              <a>Log in</a>
-            </li>
+            <Link href='/sign_up'>
+              <a>Sign up</a>
+            </Link>
           </ul>
         </nav>
       </div>
