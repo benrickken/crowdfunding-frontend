c0c65b2ac84bf8ffc071bf9203a38e91c71bc029
benrickken <benrickken@gmail.com>
2021-04-05 16:29:36 +0900

    Add links to Header and implement log out feature

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,18 @@ import { useContext } from 'react'
 import Link from 'next/Link'
 import styles from './Header.module.scss'
 import { AuthContext } from '../contexts/Auth'
+import firebase from '../utils/Firebase'
 
 export default function Header() {
   const { currentUser } = useContext(AuthContext)
+
+  const logOut = async () => {
+    try {
+      await firebase.auth().signOut()
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <header className={styles.header}>
@@ -19,13 +28,23 @@ export default function Header() {
         <nav className={styles.headerNav}>
           <ul>
             {currentUser ? (
-              <Link href='/'>
-                <a>{currentUser.email}</a>
-              </Link>
+              <>
+                <Link href='/'>
+                  <a>{currentUser.email}</a>
+                </Link>
+                <a style={{ cursor: 'pointer' }} onClick={logOut}>
+                  Log out
+                </a>
+              </>
             ) : (
-              <Link href='/sign_up'>
-                <a>Sign up</a>
-              </Link>
+              <>
+                <Link href='/log_in'>
+                  <a>Log in</a>
+                </Link>
+                <Link href='/sign_up'>
+                  <a>Sign up</a>
+                </Link>
+              </>
             )}
           </ul>
         </nav>
