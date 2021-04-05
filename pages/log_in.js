a70354d3064e1c3951487aa04905f324fb2ba055
benrickken <benrickken@gmail.com>
2021-04-05 16:49:01 +0900

    Fix pages/log_in function name

--- pages/log_in.js
+++ pages/log_in.js
@@ -1,7 +1,7 @@
 import Header from '../components/Header'
 import LogInForm from '../components/LogInForm'
 
-export default function SignUp() {
+export default function LogIn() {
   return (
     <>
       <Header />
