82369dfcd608e212298f71b5c0c57a53398ca028
benrickken <benrickken@gmail.com>
2021-05-07 21:01:32 +0900

    Fix next/Link to next/link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import Link from 'next/Link'
+import Link from 'next/link'
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
