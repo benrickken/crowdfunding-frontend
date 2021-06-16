fa73e89027d81b1ea14963e2cbd571e675171e1f
benrickken <benrickken@gmail.com>
2021-06-16 18:31:44 +0900

    Remove redundant import

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,5 +1,4 @@
 import { format } from 'date-fns'
-import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
